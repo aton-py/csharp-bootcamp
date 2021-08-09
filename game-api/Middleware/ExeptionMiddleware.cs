dfd4b94d27aea770542eef781a81daf695e0b7f0
aton-py <dornfeld.in@live.com>
2021-08-09 10:30:23 -0300

    Implementação de ExeptionMiddleware

---
+++ game-api/Middleware/ExeptionMiddleware.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace game_api.Middleware
+{
+    public class ExeptionMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch
+            {
+                await HandleExceptionAsync(context);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await context.Response.WriteAsJsonAsync(new { Message = "Error, try later" });
+        }
+    }
+}
