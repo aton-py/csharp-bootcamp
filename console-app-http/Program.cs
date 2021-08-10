9ded5ed44d358aa882c21f3dcf5bb21466be8944
aton-py <dornfeld.in@live.com>
2021-08-10 15:01:14 -0300

    Finalização de aplicação de console chamando API

--- console-app-http/Program.cs
+++ console-app-http/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace console_app_http
+var services = new ServiceCollection();
+services.AddHttpClient<ChuckNorrisClient>(c => c.BaseAddress = new Uri("https://api.chucknorris.io/"));
+var serviceProvider = services.BuildServiceProvider();
+
+var client = serviceProvider.GetService<ChuckNorrisClient>();
+var response = await client.GetRandomJokeAsync();
+
+System.Console.WriteLine($"Id: {response.Id}");
+System.Console.WriteLine($"Joke: {response.Value}");
+
+public record ChuckNorrisJoke(string Id, string Value, string Url);
+
+public class ChuckNorrisClient
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    private readonly HttpClient _httpClient;
+
+    public ChuckNorrisClient(HttpClient httpClient) => _httpClient = httpClient;
+
+    public async Task<ChuckNorrisJoke> GetRandomJokeAsync() => await _httpClient.GetFromJsonAsync<ChuckNorrisJoke>("jokes/random");
 }
