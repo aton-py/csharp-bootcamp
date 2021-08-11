bee98a37e33c58ad4389add43dc1c2ec43b67f65
aton-py <dornfeld.in@live.com>
2021-08-11 15:17:29 -0300

    tweet

---
+++ math-challenges/tweet/tweet.cs
@@ -0,0 +1,15 @@
+using System;
+
+class minhaClasse
+{
+    static void Main(string[] args)
+    {
+        string v = Console.ReadLine();
+        char[] arr = v.ToCharArray();
+
+        if (arr.Length <= 140)
+            Console.WriteLine("TWEET");
+        else
+            Console.WriteLine("MUTE");
+    }
+}
